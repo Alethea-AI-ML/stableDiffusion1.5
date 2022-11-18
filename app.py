a5fe2de2548903fc8459cf6ff69902e60ab09ef6
ajayjatav777 <107104050+ajayjatav777@users.noreply.github.com>
2022-11-18 23:42:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 import base64
 from io import BytesIO
 import os
-
+xyz=0
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
@@ -20,7 +20,7 @@ def init():
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
-    xyz=0
+    
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
   
